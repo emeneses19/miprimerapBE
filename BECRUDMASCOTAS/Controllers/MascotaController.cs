e435a05355cc6b9f571156e553b60a48036495de
unknown <menesetaipe19@gmail.com>
2024-01-07 20:15:56 -0500

    primer commit backend

---
+++ BECRUDMASCOTAS/Controllers/MascotaController.cs
@@ -0,0 +1,34 @@
+﻿using BECRUDMASCOTAS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BECRUDMASCOTAS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotaController : ControllerBase
+    {
+        private readonly AplicationDbContext _contex; 
+        public MascotaController(AplicationDbContext contex)
+        {
+            _contex = contex;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listaMascotas = await _contex.mascotas.ToListAsync();
+                return Ok(listaMascotas);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+      
+        }
+    }
+}
---
+++ BECRUDMASCOTAS/Models/AplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BECRUDMASCOTAS.Models
+{
+    public class AplicationDbContext: DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Mascota> mascotas { get; set; }
+    }
+}
